0dd7662b56c511e22b50f51c3fda1fab9f2b1fa1
Ederlo Rodrigo de Oliveira <ederlo.contato@gmail.com>
2018-10-23 23:06:17 -0300

    fix routers

--- src/routes/index.js
+++ src/routes/index.js
@@ -1,5 +1,5 @@
 import React from "react";
-import { Router,Switch, Route, Redirect } from 'react-router'
+import { Router, Switch, Route, Redirect } from 'react-router-dom'
 
 import history from './history';
 
